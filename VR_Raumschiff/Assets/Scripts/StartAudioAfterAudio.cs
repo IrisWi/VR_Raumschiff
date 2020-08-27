3fcb5e9718087f6124b8582584911bb8463d52d8
BHeister <64013227+BHeister@users.noreply.github.com>
2020-08-27 12:09:03 +0200

    Eingangsszene fertig

---
+++ VR_Raumschiff/Assets/Scripts/StartAudioAfterAudio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Media;
+using UnityEngine;
+
+public class StartAudioAfterAudio : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Play", 15);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        
+        
+    }
+
+    void Play()
+    {
+        audioSource.Play();
+    }
+
+  
+}

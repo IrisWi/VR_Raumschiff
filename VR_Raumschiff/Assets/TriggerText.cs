4034e0151be0bdd8cc1cf17f3faac84e6bfb9425
CarolinKoen <50081724+CarolinKoen@users.noreply.github.com>
2020-08-28 17:45:41 +0200

    Text

---
+++ VR_Raumschiff/Assets/TriggerText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerText : MonoBehaviour
+{
+	public GameObject uiObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+		uiObject.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider player)
+    {
+		if (player.gameObject.tag == "Player")
+		{
+			uiObject.SetActive(true);
+			StartCoroutine("WaitForSec");
+		}
+        
+    }
+
+	IEnumerator WaitForSec()
+	{
+		yield return new WaitForSeconds(5);
+		Destroy(uiObject);
+		Destroy(GameObject);
+	}
+}
